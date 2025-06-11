cc5373e59e9ed5e3396746a87e05c7ce524d2860
Fets7 <143446190+Fets7@users.noreply.github.com>
2025-06-11 14:58:28 +0200

    Delete Fesi.AI/Fesi.AI directory

--- Fesi.AI/Fesi.AI/js/hamburger.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-    const overlay = document.getElementById('overlay');
-    const body = document.body;
-
-    overlay.classList.toggle('active');
-    body.classList.toggle('menu-open'); // Hiermee verbergen we de rest van de pagina
-}
